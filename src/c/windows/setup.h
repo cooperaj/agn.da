35254ef5ef41f3e4105fbabf6d7c4e746b35e044
Adam Cooper <adam@networkpie.co.uk>
2016-10-04 13:15:08 +0100

    Setup the configuration site under github pages.

--- src/c/windows/setup.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <pebble.h>
-
-#define DIALOG_SETUP_WINDOW_APP_NAME "Agn.da"
-#define DIALOG_SETUP_WINDOW_MESSAGE  "Set up in the\nPebble app"
-
-void dialog_setup_window_push();
