3bd1e446859239570780d03b01dba20aefc41ebd
Adam Cooper <adam@networkpie.co.uk>
2016-10-04 17:28:04 +0100

    An attempt to get configuration working.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Agn.da
 =========
+
+A Pebble timeline application that consumes the Joind.in API to inform you of  
+your conference and talk agendas.
--- src/c/main.c
+++ src/c/main.c
@@ -3,28 +3,28 @@
 #include "windows/setup.h"
 
 // A struct for our specific settings (see main.h)
-ClaySettings settings;
+AppSettings settings;
 
 static bool loadSettings() {
   // Read settings from persistent storage, if they exist
   persist_read_data(1, &settings, sizeof(settings));
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Load settings: Event_ID: %s", settings.Event_ID);
-  
-  return false;
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Load settings: SetupComplete: %s", settings.SetupComplete ? "true" : "false");
+
+  return !settings.SetupComplete;
 }
 
 static void saveSettings() {
   persist_write_data(1, &settings, sizeof(settings));
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Save settings: Event_ID: %s", settings.Event_ID);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Save settings: SetupComplete: %s", settings.SetupComplete ? "true" : "false");
 }
 
 static void inboxReceivedHandler(DictionaryIterator *iter, void *context) {
   // Read event id
-  Tuple *event_id = dict_find(iter, MESSAGE_KEY_Event_ID);
-  if (event_id) {
-    memcpy(settings.Event_ID, event_id->value->cstring, event_id->length);
+  Tuple *setup_complete = dict_find(iter, MESSAGE_KEY_SetupComplete);
+  if (setup_complete) {
+      settings.SetupComplete = setup_complete->value->uint8;
   }
-  
+
   saveSettings();
 }
 
@@ -32,7 +32,7 @@ static void init() {
   // Open AppMessage connection
   app_message_register_inbox_received(inboxReceivedHandler);
   app_message_open(128, 128);
-  
+
   if (loadSettings()) {
     // dialog_ready_window_push();
   } else {
